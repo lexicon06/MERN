0e5fb4f2f72eab4f842ff3981da35cd6a3396c28
lexicon06 <pablo.hehe2@gmail.com>
2024-03-11 03:35:28 -0300

    fixed config

--- bookstore-mern-stack/backend/config.js
+++ bookstore-mern-stack/backend/config.js
@@ -1,3 +1,2 @@
 export const PORT = 5555;
-
 export const mongoDBURL = "mongodb+srv://pablohehe2:mongonft@cluster0.v7esaru.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0";
